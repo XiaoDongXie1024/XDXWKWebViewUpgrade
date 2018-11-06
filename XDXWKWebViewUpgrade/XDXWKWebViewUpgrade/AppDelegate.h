28340c18c3b1ab02cbc1f909a94a7ad4895285d5
Demon <demonli@tvunetworks.com>
2018-11-06 15:37:43 +0800

    Create Project

---
+++ XDXWKWebViewUpgrade/XDXWKWebViewUpgrade/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XDXWKWebViewUpgrade
+//
+//  Created by 小东邪 on 2018/11/6.
+//  Copyright © 2018 小东邪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XDXWKWebViewUpgrade/XDXWKWebViewUpgrade/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XDXWKWebViewUpgrade
+//
+//  Created by 小东邪 on 2018/11/6.
+//  Copyright © 2018 小东邪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
